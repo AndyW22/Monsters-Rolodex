2c14da8e697d60622b39cb0f47b6afa05364f0e3
Andy <andrew.waller24@gmail.com>
2020-12-20 14:14:25 +0000

    empty2

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import CardList from './components/card-list/card-list';
 import './App.css';
-import Search from "./components/search";
-function App() {
-  const [text, setText] = React.useState([]);
-  const [searchField, setsearchField] = React.useState('');
-  const [loading, setLoading] = React.useState(true);
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setText(users));
-    setLoading(false);
-  });
+import Search from './components/search';
 
-  const filteredmonsters = text.filter((monster) =>
-    monster.name.toLowerCase().includes(searchField));
+function App() {
   return (
     <div className="App">
-      <input
-        type="search"
-        placeholder="search"
-        onChange={(e) => {
-          setsearchField(e.target.value.toLowerCase());
-        }}
-      />
-      {loading && <h2>Loading...</h2>}
-      <CardList monsters={filteredmonsters} />
+      <Search />
     </div>
   );
 }
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import CardList from './card-list/card-list';
+import '../App.css';
 
 export default () => {
-    
-}
+  const [text, setText] = React.useState([]);
+  const [searchField, setsearchField] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setText(users));
+  });
+
+  const filteredmonsters = text.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField));
+
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="search"
+        onChange={(e) => {
+          setsearchField(e.target.value.toLowerCase());
+        }}
+      />
+      <CardList monsters={filteredmonsters} />
+    </div>
+  );
+};
